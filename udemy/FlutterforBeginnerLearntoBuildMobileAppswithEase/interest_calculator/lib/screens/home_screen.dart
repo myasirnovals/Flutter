acc6dd767aab1ae87be21c93337930b0232563a3
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-07-21 14:07:42 +0700

    add logic for interest calculator app

--- udemy/FlutterforBeginnerLearntoBuildMobileAppswithEase/interest_calculator/lib/screens/home_screen.dart
+++ udemy/FlutterforBeginnerLearntoBuildMobileAppswithEase/interest_calculator/lib/screens/home_screen.dart
@@ -8,7 +8,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var _currencies = ["Dollar", "Rupees", "Pound"];
+  var _formKey = GlobalKey<FormState>();
+
+  var _currencies = ["Dollar", "Rupiah", "Pound"];
+  var _currentSelectedItem = "Rupiah";
+  var displayResult = "";
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,94 +29,166 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.all(10.0),
-        children: [
-          Image.asset(
-            "assets/images/finance-icon.png",
-            width: 300.0,
-          ),
-          TextField(
-            decoration: InputDecoration(
-                labelText: "Principle Amount",
-                hintText: "Enter a principal amount e.g 15000",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                )),
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          TextField(
-            decoration: InputDecoration(
-                labelText: "Rate of Interest",
-                hintText: "Enter in percentage e.g 15",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                )),
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                      labelText: "Term",
-                      hintText: "Term in Years e.g 10",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      )),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: [
+            Image.asset(
+              "assets/images/finance-icon.png",
+              width: 300.0,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              controller: principalController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a principal amount";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  labelText: "Principle Amount",
+                  hintText: "Enter a principal amount e.g 15000",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  )),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              controller: roiController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a Rate of Interest";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  labelText: "Rate of Interest",
+                  hintText: "Enter in percentage e.g 15",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  )),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: termController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter a Term";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        labelText: "Term",
+                        hintText: "Term in Years e.g 10",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Expanded(
-                child: DropdownButton(
-                  items: _currencies.map((value) {
-                    return DropdownMenuItem(child: Text(value), value: value);
-                  }).toList(),
-                  onChanged: (newSelectedValue) {},
-                  value: "Dollar",
+                SizedBox(
+                  width: 10.0,
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "calculate",
-                  style: TextStyle(color: Colors.redAccent),
+                Expanded(
+                  child: DropdownButton(
+                    items: _currencies.map((value) {
+                      return DropdownMenuItem(child: Text(value), value: value);
+                    }).toList(),
+                    onChanged: (newSelectedValue) {
+                      setState(() {
+                        this._currentSelectedItem = newSelectedValue!;
+                      });
+                    },
+                    value: _currentSelectedItem,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Reset",
-                  style: TextStyle(color: Colors.redAccent),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        this.displayResult = calculateInterest();
+                      }
+                    });
+                  },
+                  child: Text(
+                    "calculate",
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Center(
-            child: Text("Result: "),
-          ),
-        ],
+                SizedBox(
+                  height: 10.0,
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      resetFields();
+                    });
+                  },
+                  child: Text(
+                    "Reset",
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.redAccent,
+                    side: BorderSide(color: Colors.redAccent),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: Text(displayResult),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  String calculateInterest() {
+    double? principal = double.tryParse(principalController.text);
+    double? roi = double.tryParse(roiController.text);
+    double? term = double.tryParse(termController.text);
+
+    if (principal == null || roi == null || term == null) {
+      return "Please enter valid numbers in all fields.";
+    }
+
+    double rateInDecimal = roi / 100;
+    double totalPayable = principal + (principal * rateInDecimal * term);
+
+    String result =
+        "After $term years, your invesment will be worth of ${totalPayable.toStringAsFixed(2)} in $_currentSelectedItem";
+
+    return result;
+  }
+
+  void resetFields() {
+    principalController.text = "";
+    roiController.text = "";
+    termController.text = "";
+
+    _currentSelectedItem = _currencies[1];
+    displayResult = "";
   }
 }
