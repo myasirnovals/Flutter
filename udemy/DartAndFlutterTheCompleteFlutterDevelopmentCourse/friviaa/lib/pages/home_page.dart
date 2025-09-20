4ffc592282af20dafc2693601571913832044e1a
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-09-20 08:35:15 +0700

    Friviaa app finish

---
+++ udemy/DartAndFlutterTheCompleteFlutterDevelopmentCourse/friviaa/lib/pages/home_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:friviaa/pages/game_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth;
+
+  double _currentDifficultyLevel = 0;
+
+  final List<String> _difficultyTexts = ['Easy', 'Medium', 'Hard'];
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: _deviceHeight! * 0.10),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                _appTitle(),
+                _difficultySlider(),
+                _startGameBtn(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _appTitle() {
+    return Column(
+      children: [
+        const Text(
+          'Friviaa',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Text(
+          _difficultyTexts[_currentDifficultyLevel.toInt()],
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _difficultySlider() {
+    return Slider(
+      label: 'Difficulty',
+      min: 0,
+      max: 2,
+      divisions: 2,
+      value: _currentDifficultyLevel,
+      onChanged: (_value) {
+        setState(() {
+          _currentDifficultyLevel = _value;
+        });
+      },
+    );
+  }
+
+  Widget _startGameBtn() {
+    return MaterialButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext _context) {
+              return GamePage(
+                difficultyLevel:
+                    _difficultyTexts[_currentDifficultyLevel.toInt()]
+                        .toLowerCase(),
+              );
+            },
+          ),
+        );
+      },
+      color: Colors.blue,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        'Start',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
